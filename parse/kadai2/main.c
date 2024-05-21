f5d483f4d0ac271a289553c4c7f79b83d1b2f774
rytst <r.saito0111@gmail.com>
2024-05-21 18:10:26 +0900

    option

--- parse/kadai2/main.c
+++ parse/kadai2/main.c
@@ -8,24 +8,44 @@
 
 #include <stdio.h>  /* fprintf */
 #include <stdlib.h> /* exit */
+#include <string.h>
 #include "regmatch.h"
 
 int debug = 0;     /* デバッグ情報を表示する(1)/しない(0) */
 char *reg_string;  /* 正規表現文字列 */
+char *option;
 
 int main(int argc, char *argv[])
 {
   /* debug = 1;*/
 
-  if (argc != 2)
+  if (argc < 2)
     fatal_error("no argument");
 
-  reg_string = argv[1];
 
   if (debug) 
     printf("Regular expression: %s\n", reg_string);
 
-  parse(); 
+  if (argc == 2) {
+    reg_string = argv[1];
+    parse();
+  } else if (argc == 3) {
+    option = argv[1];
+    reg_string = argv[2];
+
+    if (strcmp(option, "-d0") == 0) {
+      printf("%s\n", reg_string);
+    } else if (strcmp(option, "-d1") == 0) {
+      lexer();
+    } else if (strcmp(option, "-d2") == 0) {
+      parse();
+    } else {
+      fatal_error("invalid option");
+	}
+  } else {
+    fatal_error("too many arguments");
+  }
+
   return 0;
 }
 
